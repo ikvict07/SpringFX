e517ef319701d457f8b9363e4b7a170978b152a4
Anton Horobets <anton.horobets@erstegroup.com>
2025-02-09 00:34:47 +0100

    Init commit

---
+++ src/main/java/org/nevertouchgrass/springfx/SpringFxApplication.java
@@ -0,0 +1,13 @@
+package org.nevertouchgrass.springfx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFxApplication.class, args);
+    }
+
+}
